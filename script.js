5b35631be47e1e3b934dfdccc1f8b8034df3e644
Odongo006 <www.henryodongo006@gmail.com>
2022-12-19 17:24:28 +0300

    Make mobile menu interactive

---
+++ script.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line linebreak-style
+const hamburger = document.querySelector('.menu-icon');
+const navMenu = document.querySelector('.nav-items');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link a').forEach((n) => (n).addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
