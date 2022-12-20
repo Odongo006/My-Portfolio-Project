4aaa513d2ecd961192db4b33358f1f5e663b0071
Odongo006 <www.henryodongo006@gmail.com>
2022-12-20 13:05:06 +0300

    Add interactive mobile menu

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line linebreak-style
 // eslint-disable-next-line linebreak-style
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.top-nav');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-});
+// eslint-disable-next-line no-unused-vars
+function myFunction() {
+  const x = document.getElementById('navTop');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+// const hamburger = document.querySelector('.hamburger');
+// const navMenu = document.querySelector('.top-nav');
 
-document.querySelectorAll('.nav-link a').forEach((n) => (n).addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
+// hamburger.addEventListener('click', () => {
+//   hamburger.classList.toggle('active');
+//   navMenu.classList.toggle('active');
+// });
+
+// document.querySelectorAll('.nav-link a').forEach((n) => (n).addEventListener('click', () => {
+//   hamburger.classList.remove('active');
+//   navMenu.classList.remove('active');
+// }));
