72cc53ea44b8269b38776b5565e712aa5ee0772f
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2022-12-23 00:47:39 +0500

    add some code in js file

--- script.js
+++ script.js
@@ -35,3 +35,50 @@ const closeModal = function () {
 };
 
 closeModalBtn.addEventListener('click', closeModal);
+
+const storeData = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign- ups required',
+    imagesrc: 'imgs/poster.svg',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    imagesrc: 'imgs/poster2.svg',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook,s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    imagesrc: 'imgs/poster 3.svg',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+  {
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    imagesrc: 'imgs/poster 4.svg',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    link: 'link_to_live_version.com',
+  },
+];
+
+const projectImgSrc = document.querySelectorAll('.tonic');
+const projectLanguagesHtml = document.querySelectorAll('.project-languages-html');
+const projectLanguagesRuby = document.querySelectorAll('.project-languages-ruby');
+const projectLanguagesCss = document.querySelectorAll('.project-languages-css');
+const projectLanguagesJs = document.querySelectorAll('.project-languages-js');
+const projectsDescription = document.querySelectorAll('.projects-description');
+
+for (let i = 0; i < projectLanguagesHtml.length;i++){
+  projectImgSrc[i].src = storeData[i].imagesrc
+  projectLanguagesHtml[i].innerHTML = storeData[i].technologies[0]
+  projectLanguagesRuby[i].innerHTML = storeData[i].technologies[1]
+  projectLanguagesCss[i].innerHTML = storeData[i].technologies[2]
+  projectLanguagesJs[i].innerHTML = storeData[i].technologies[3]
+  projectsDescription[i].innerHTML = storeData[i].description
+}
